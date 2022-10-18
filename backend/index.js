f0e0eecbb5ee8709827c5ff309ac5d885c0850fd
bhuvan <bhuvanchau@gmail.com>
2022-10-17 21:03:56 -0400

    v2

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,18 @@
-import app from './server.js'
-
-import mongodb from "mongodb"
-import dotenv from "dotenv"
-import MoviesDAO from './dao/moviesDAO.js'
-/** aysnc function to connect to mongodb clusster */
+import mongodb from "mongodb";
+import dotenv from "dotenv";
 async function main() {
-
-    dotenv.config() /**
-     loading in environment variables* 
-     */
-    const client = new mongodb.MongoClient(
-        process.env.MOVIEREVIEWS_DB_URL
-    )
-    /**
-     creating an instance of mongocleint 
-     */
-    const port = process.env.PORT || 8000
-    try {
-        await client.connect()
-        await MoviesDAO.injectDB(client)
-        app.listen(port, () => {
-            console.log('server is running on port:' + port);
-        })
-
-    } catch (e) {
-        console.error(e);
-        process.exit(1)
-    }
+  dotenv.config();
+  const client = new mongodb.MongoClient(process.env.MOVIEREVIEWS_DB_URL);
+  const port = process.env.PORT || 8000;
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+    app.listen(port, () => {
+      console.log("server is running on port:" + port);
+    });
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
 }
-
 main().catch(console.error);
