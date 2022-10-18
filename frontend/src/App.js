1ed545d7db08dbbe70b5f9c2ee4834b6b7bd3e0d
bhuvan <bhuvanchau@gmail.com>
2022-10-18 05:45:12 -0400

    front end working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import AddReview from "./components/add-review";
@@ -8,20 +10,50 @@ import Movie from "./components/movie";
 import Login from "./components/login";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-
 function App() {
+  const [user, setUser] = React.useState(null);
+  async function login(user = null) {
+    setUser(user);
+  }
+  async function logout() {
+    setUser(null);
+  }
   return (
     <div className="App">
+      home
       <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home"> Movie Review App</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#home">
+              <Link to={"/movies"}>Movies</Link>
+            </Nav.Link>
+            <Nav.Link href="#link">
+              {user ? (
+                <a onClick={logout}>Logout User</a>
+              ) : (
+                <Link to={"/login"}>Login</Link>
+              )}
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
+      <Routes>
+        <Route exact path={["/", "/movies"]} component={MoviesList}></Route>
+        <Route
+          path="/movies/:id/review"
+          render={(props) => <AddReview {...props} user={user} />}
+        ></Route>
+        <Route
+          path="/movies/:id/"
+          render={(props) => <Movie {...props} user={user} />}
+        ></Route>
+        <Route
+          path="/login"
+          render={(props) => <Login {...props} login={login} />}
+        ></Route>
+      </Routes>
     </div>
   );
 }
--- frontend/src/components/movies-list.js
+++ frontend/src/components/movies-list.js
@@ -3,3 +3,5 @@ import React from "react";
 function MoviesList() {
   return <div className="app">Movies List</div>;
 }
+
+export default MoviesList
